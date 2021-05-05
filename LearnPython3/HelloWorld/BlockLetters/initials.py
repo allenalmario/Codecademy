300db6f16f30ff9471cd1dfbc2b0d4705d561f2f
allenalmario <allenalmario@gmail.com>
2021-05-05 12:07:55 -0700

    first

---
+++ LearnPython3/HelloWorld/BlockLetters/initials.py
@@ -0,0 +1,11 @@
+# First name: Allen
+# Last name: Almario
+# fun fact: My hockey team is the Golden Knights
+print("  A  " + "  " + "  A  ")
+print(" A A " + "  " +  " A A ")
+print("A   A" + "  " +  "A   A")
+print("AAAAA" + "  " +  "AAAAA")
+print("A   A" + "  " +  "A   A")
+print("A   A" + "  " +  "A   A")
+print("A   A" + "  " +  "A   A")
+print("A   A" + "  " +  "A   A")
