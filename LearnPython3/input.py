b1c6b278cefcd58172521068f9961733248664f8
allenalmario <allenalmario@gmail.com>
2021-05-05 13:09:40 -0700

    added input program

---
+++ LearnPython3/input.py
@@ -0,0 +1,8 @@
+# simple program that asks user for their name and favorite color
+# outputs a message according to they input
+
+name = input("What is your name: ")
+print("Hello, " + name)
+
+favorite_color = input("What is your favorite color: ")
+print(favorite_color + " is a really nice color!")
